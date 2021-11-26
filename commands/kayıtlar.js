e0c07e4433ab1e2c09373d444efa4f63c49e366b
Wolyo <93871074+Wolyo-JS@users.noreply.github.com>
2021-11-26 20:17:01 +0400

    Update kayıtlar.js

--- commands/kayıtlar.js
+++ commands/kayıtlar.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
   if (!member) return message.channel.send(embed.setDescription("Geçerli bir üye belirtmelisin!")).then(x => x.delete({ timeout: 5000 }));
   let memberData = await kayitlar.findById(member.id);
   if (!memberData) return message.channel.send(embed.setDescription(`${member} üyesinin kayıt geçmişi bulunamadı!`));
-  message.channel.send(embed.setDescription(`${member} üyesinin kayıt geçmişi; **(son 10)**\n\n${memberData.kayitlar.reverse().slice(0, 10).map((data, index) => `\`${index+1}.\` ${data.isim} (${data.roller.map(r => `<@&${r}>`).join(", ")}) | ${new Date(data.tarih).toTurkishFormat()}`).join("\n")}`));
+  message.channel.send(embed.setDescription(`${member} üyesinin kayıt geçmişi; **(son 10)**\n\n${memberData.kayitlar.reverse().slice(0, 10).map((data, index) => `\`${index+1}.\` \`${data.isim}\` (${data.roller.map(r => `<@&${r}>`).join(", ")}) | \`${new Date(data.tarih).toTurkishFormat()}`).join("\n")}\``));
 };
 
 module.exports.config = {
